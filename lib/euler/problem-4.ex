d2aba2e62747c8c4fd7b43baff4f55969eda0bb3
Erik J. Sturcke <erik@carbonbox.com>
2014-02-26 01:16:09 -0500

    Add problem statement for problem 4

--- lib/euler/problem-4.ex
+++ lib/euler/problem-4.ex
@@ -1,5 +1,16 @@
 defmodule Euler.Problem4 do
 
+  @moduledoc """
+  # Largest palindrome product
+
+  [Problem 4](http://projecteuler.net/problem=4)
+
+  A palindromic number reads the same both ways. The largest palindrome made
+  from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+  Find the largest palindrome made from the product of two 3-digit numbers.
+  """
+
   defp isPalindrome(n), do: to_string n == String.reverse to_string n
 
   defp merge(a, b) do
