8ec36b05b5ec29974885067179a3820497525abf
Erik J. Sturcke <erik@carbonbox.com>
2014-02-26 01:14:29 -0500

    Add problem description for problem 3

--- lib/euler/problem-3.ex
+++ lib/euler/problem-3.ex
@@ -1,5 +1,15 @@
 defmodule Euler.Problem3 do
 
+  @moduledoc """
+  # Largest prime factor
+
+  [Problem 3](http://projecteuler.net/problem=3)
+  
+  The prime factors of 13195 are 5, 7, 13 and 29.
+
+  What is the largest prime factor of the number 600851475143?
+  """
+
   import Euler.Math.Factors
 
   def solve do
