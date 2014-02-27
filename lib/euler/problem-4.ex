f02c73d052bdfeb2f43c936afb89c8381587d143
Erik J. Sturcke <erik@carbonbox.com>
2014-02-27 00:17:42 -0500

    Try to fix code for problem 4

--- lib/euler/problem-4.ex
+++ lib/euler/problem-4.ex
@@ -9,19 +9,39 @@ defmodule Euler.Problem4 do
   from the product of two 2-digit numbers is 9009 = 91 × 99.
 
   Find the largest palindrome made from the product of two 3-digit numbers.
+
+  ## Solution
+
+    nm = (100a + 10b + c)(100d + 10e + f) 
+       = 10000ad + 1000(ae + cd) + 100(af + be + cd) + 10(bf + ce) + cf
+       = 
+
+  The product will result in a six-digit palindrome `abccda` that can be written as
+
+    nm = 100000a + 10000b + 1000c + 100c + 10b + a
+       = 100001a + 10010b + 1100c
+       = 11 (9091a + 910b + 100c)
+
+  And so either *n* or *m* must be divisible by 11.
+
+  If we assume that the largest palindrome starts with a 9, then we have
+
+    nm = 11 (81819 + 910b + 100c)
+
   """
 
-  defp isPalindrome(n), do: to_string n == String.reverse to_string n
+  defp palindrome?(word) when is_bitstring(word), do: word == String.reverse word
+  defp palindrome?(n)    when is_number(n),       do: palindrome? to_string n
 
-  defp merge(a, b) do
+  defp split(stream), do: { Enum.at(stream, 0), Stream.drop(stream, 1) }
+
+  def merge(a, b) do
     cond do
       Enum.empty? a -> b
       Enum.empty? b -> a
       true          -> (
-        x  = a |> Enum.at 0
-        y  = b |> Enum.at 0
-        xs = a |> Stram.drop 1
-        ys = b |> Stram.drop 1
+        {x, xs} = split a
+        {y, ys} = split a
         if x > y do
           Stream.concat([x], merge(xs, b))
         else
@@ -31,12 +51,21 @@ defmodule Euler.Problem4 do
     end
   end
 
-  defp products(a, b) do
-    0
+  def products(a, b) when a == b, do: [ b * b ]
+  def products(a, b) do
+    first = b * b
+    rest  = Stream.Lazy[fun: fn(f) -> 
+    Stream.
+    IO.puts "products of #{a} and #{b}"
+    row  = b-1..a-1 |> Stream.map fn(n) -> n * 2 end
+    rest = products a, b - 1
+    Stream.concat([b * b], merge(row, rest)) 
   end
 
   def solve do
-    0
+    products(100, 999)
+    |> Stream.filter(fn(n) -> palindrome? n end)
+    |> Stream.take 1
   end
 
 end
