51af7cde7322f6e021786f1b36310035e4f309a2
Erik J. Sturcke <erik@sturcke.co>
2015-10-24 21:46:38 -0400

    Solve problem 8

--- test/euler_test.exs
+++ test/euler_test.exs
@@ -11,5 +11,6 @@ defmodule EulerTest do
   test "problem 4", do: assert "d4cfc27d16ea72a96b83d9bdef6ce2ec" == Euler.solve(4) |> md5
   test "problem 5", do: assert "bc0d0a22a7a46212135ed0ba77d22f3a" == Euler.solve(5) |> md5
   test "problem 6", do: assert "867380888952c39a131fe1d832246ecc" == Euler.solve(6) |> md5
-
+  test "problem 7", do: assert "8c32ab09ec0210af60d392e9b2009560" == Euler.solve(7) |> md5
+  test "problem 8", do: assert "0f53ea7949d32ef24f9186207600403c" == Euler.solve(8) |> md5
 end
