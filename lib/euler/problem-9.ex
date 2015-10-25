b135052efc6677ef639587933db40e2468f073f0
Erik J. Sturcke <erik@sturcke.co>
2015-10-25 09:09:28 -0400

    Set up problem 9

---
+++ lib/euler/problem-9.ex
@@ -0,0 +1,22 @@
+defmodule Euler.Problem9 do
+
+  @moduledoc """
+  # Special Pythagorean triplet
+
+  [Problem 9](http://projecteuler.net/problem=9)
+
+  A Pythagorean triplet is a set of three natural numbers, *a* < *b* < *c*, for which,
+  
+    *a*² + *b*² = *c*²
+
+  For example, 3² + 4² = 9 + 16 = 25 = 5².
+  
+  There exists exactly one Pythagorean triplet for which *a* + *b* + *c* = 1000.
+  Find the product *abc*.
+  """
+
+  def solve do
+    0
+  end
+
+end
